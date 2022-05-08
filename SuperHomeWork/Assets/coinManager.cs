5bb62cc6125efcb43539e4c7781c39454f7498fc
yanashamis <shamis.yana@gmail.com>
2022-05-08 13:28:52 +0300

    start

---
+++ SuperHomeWork/Assets/coinManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class coinManager : MonoBehaviour
+{
+    //------------------------------test animated coin
+    [SerializeField] GameObject animatedCoinPrefab;
+    [SerializeField] Transform target;
+    public Transform collectedCoinPosition;
+
+
+    [Space]
+    [Header("availebal coins")]
+    [SerializeField] int maxCoins;
+    Queue<GameObject> coinsqueue = new Queue<GameObject>();
+
+    [Space]
+    [Header("animation settings")]
+    [SerializeField] [Range(0.3f, 0.6f)] float minAnimDuraction;
+    [SerializeField] [Range(0.9f, 1.5f)] float maxAnimDuraction;
+
+    Vector3 targetPosition;
+
+    [SerializeField] Ease easeType;
+    [SerializeField] float spread;
+
+    private void Awake()
+    {
+        targetPosition = target.position;
+        prepareCoins();
+   
+    }
+    //-----------------------test1
+    public void lastButton()
+    {
+        while (coinsqueue.Count > 0) { 
+            GameObject coin = coinsqueue.Dequeue();
+        coin.SetActive(true);
+
+        coin.transform.position = collectedCoinPosition.position; //+ new Vector3(Random.Range(-spread, spread), 0f, 0f);
+        float duration = Random.Range(minAnimDuraction, maxAnimDuraction);
+
+        coin.transform.DOMove(targetPosition, duration)
+                .SetEase(easeType)
+                .OnComplete(() =>
+                {
+                    coin.SetActive(false);
+                    coinsqueue.Enqueue(coin);
+                });
+        }
+    }
+
+
+
+    void prepareCoins()
+    {
+
+        GameObject coin;
+        for (int i = 0; i < maxCoins; i++)
+        {
+
+            coin = Instantiate(animatedCoinPrefab);
+            coin.transform.parent = transform;
+            coin.SetActive(false);
+            coinsqueue.Enqueue(coin);
+        }
+
+
+    }
+
+}
---
+++ SuperHomeWork/Assets/uiHolder.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class uiHolder : MonoBehaviour
+{
+    public coinManager CoinManager;
+    private RawImage barRawImage;
+    private RectTransform barMaskrectTransform;
+    public float speed;
+    public const float maxWidthOfBar = 600;
+    public GameObject edge;
+
+    private float random()
+    {
+        float a = Random.Range(10f, 90f);
+        return a;
+    }
+
+    public void ChangeState(float percents)
+    {
+        if ((percents > 100) || (percents < 0))
+            return;
+            StartCoroutine(ChangeStateCor(percents));
+    }
+
+    public void RandomButton1()
+    {
+      //  StartCoroutine(ChangeState(random()));
+      //StartCoroutine(RandomButton());
+    }
+
+    IEnumerator RandomButton()
+    {
+        yield return new WaitForEndOfFrame();
+        ChangeStateCor(random());
+    }
+
+    IEnumerator ChangeStateCor(float percents)
+    {
+        yield return new WaitForEndOfFrame();
+       // if (barMaskrectTransform.sizeDelta.x < maxWidthOfBar * percents / 100)
+         if(Mathf.Abs(barMaskrectTransform.sizeDelta.x - maxWidthOfBar * percents / 100) > speed)
+        {
+            if(barMaskrectTransform.sizeDelta.x < maxWidthOfBar * percents / 100)
+               barMaskrectTransform.sizeDelta += new Vector2(speed, 0);
+            else
+               barMaskrectTransform.sizeDelta -= new Vector2(speed, 0);
+               StartCoroutine(ChangeStateCor(percents));
+        }
+        
+        else if (percents == 100)
+        {
+            CoinManager.lastButton();
+        }
+
+
+    }
+
+
+    private Image barImage;
+    private void Awake()
+    {
+
+        barMaskrectTransform = transform.Find("barMask").GetComponent<RectTransform>();
+        barRawImage = transform.Find("barMask").Find("bar").GetComponent<RawImage>();   
+    }
+
+
+
+    private void Update()
+    {
+        Rect uvRect = barRawImage.uvRect;
+        uvRect.x -= .2f * Time.deltaTime;
+        barRawImage.uvRect = uvRect;
+
+    }
+
+}
